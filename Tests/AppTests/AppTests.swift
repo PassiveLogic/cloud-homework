5a888f67b881e54fb605bc4bc8f97e256375a7d1
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-05-24 15:29:51 -0600

    test: Adds GraphQL tests

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,6 +1,7 @@
 @testable import App
 import XCTVapor
 import Fluent
+import GraphQL
 
 final class AppTests: XCTestCase {
     var app: Application!
@@ -23,4 +24,40 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+
+    func testGraphQLTodos() async throws {
+        let testTodos = [Todo(title: "test1"), Todo(title: "test2")]
+        try await testTodos.create(on: app.db)
+
+        try await self.app.test(.POST, "graphql", beforeRequest: { req in
+            try req.content.encode(
+                GraphQLRequest(
+                    query: """
+                    {
+                        todos {
+                            title
+                        }
+                    }
+                    """
+                )
+            )
+        }, afterResponse: { res async throws in
+            XCTAssertEqual(res.status, .ok)
+            let result = try res.content.decode(GraphQLResult.self)
+            XCTAssertEqual(
+                result,
+                GraphQLResult(
+                    data: [
+                        "todos": [
+                            ["title": "test1"],
+                            ["title": "test2"],
+                        ]
+                    ],
+                    errors: []
+                )
+            )
+        })
+    }
 }
+
+extension GraphQLRequest: Content {}
