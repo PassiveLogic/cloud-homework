9a98781ddb1a2087194981293966e267d6f61a76
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-05-24 15:46:34 -0600

    test: Adds GraphQL API tests

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -58,6 +58,101 @@ final class AppTests: XCTestCase {
             )
         })
     }
+
+    func testGraphQLCreateTodo() async throws {
+        var id: UUID? = nil
+        try await self.app.test(.POST, "graphql", beforeRequest: { req in
+            try req.content.encode(
+                GraphQLRequest(
+                    query: """
+                    mutation {
+                        createTodo(title: "test1") {
+                            id
+                        }
+                    }
+                    """
+                )
+            )
+        }, afterResponse: { res async throws in
+            XCTAssertEqual(res.status, .ok)
+            let result = try res.content.decode(GraphQLResult.self)
+            let idString = try XCTUnwrap(result.data?["createTodo"].dictionary?["id"]?.string)
+            id = try XCTUnwrap(UUID(uuidString: idString))
+        })
+        
+        let todoOptional = try await Todo.find(XCTUnwrap(id), on: app.db)
+        let todo = try XCTUnwrap(todoOptional)
+        XCTAssertEqual(todo.title, "test1")
+    }
+    
+    func testGraphQLUpdateTodo() async throws {
+        let todo = Todo(title: "test1")
+        try await todo.create(on: app.db)
+
+        try await self.app.test(.POST, "graphql", beforeRequest: { req in
+            try req.content.encode(
+                GraphQLRequest(
+                    query: """
+                    mutation {
+                        updateTodo(id: "\(XCTUnwrap(todo.id))", title: "test2") {
+                            title
+                        }
+                    }
+                    """
+                )
+            )
+        }, afterResponse: { res async throws in
+            XCTAssertEqual(res.status, .ok)
+            let result = try res.content.decode(GraphQLResult.self)
+            XCTAssertEqual(
+                result,
+                GraphQLResult(
+                    data: [
+                        "updateTodo": [
+                            "title": "test2",
+                        ]
+                    ],
+                    errors: []
+                )
+            )
+        })
+    }
+    
+    func testGraphQLDeleteTodo() async throws {
+        let todo = Todo(title: "test1")
+        try await todo.create(on: app.db)
+
+        try await self.app.test(.POST, "graphql", beforeRequest: { req in
+            try req.content.encode(
+                GraphQLRequest(
+                    query: """
+                    mutation {
+                        deleteTodo(id: "\(XCTUnwrap(todo.id))") {
+                            title
+                        }
+                    }
+                    """
+                )
+            )
+        }, afterResponse: { res async throws in
+            XCTAssertEqual(res.status, .ok)
+            let result = try res.content.decode(GraphQLResult.self)
+            XCTAssertEqual(
+                result,
+                GraphQLResult(
+                    data: [
+                        "deleteTodo": [
+                            "title": "test1",
+                        ]
+                    ],
+                    errors: []
+                )
+            )
+        })
+        
+        let todoOptional = try await Todo.find(XCTUnwrap(todo.id), on: app.db)
+        XCTAssertNil(todoOptional)
+    }
 }
 
 extension GraphQLRequest: Content {}
